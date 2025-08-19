5498d9f0fca18a29c579800aa9bff4b0d8fec38a
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-08-19 18:59:28 +0530

    just modify

--- Array/src/LinearSearch.java
+++ Array/src/LinearSearch.java
@@ -1,47 +1,47 @@
 public class LinearSearch {
     public static void main(String[] args) {
         //Linear search
-        int[] arr = {21,24,19,56,74,3,6,98,11,};
+        int[] arr = {21,24,16,29,56,74,3,6,98,11,};
         int target = 56;
-        int ans = linearSearch2(arr,target);
+        int ans = linearSearch(arr,target);
         System.out.println(ans);
 
     }
-    //search the target and return true or false
-    static boolean linearSearch3(int[] arr, int target){
-        if (arr == null) {
+
+    // search the target and return true or false
+    static boolean linearSearch3(int[] arr, int target) {
+        if (arr.length == 0) {
             return false;
         }
 
-        //for loop for search
-        for(int element : arr){
-            if(element == target){
+        // run a for loop
+        for (int element : arr) {
+            if (element == target) {
                 return true;
             }
         }
-
-        //this statement will execute if the none of the above return statement is execute
-        //hence the target not found
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
         return false;
     }
 
-    //search the target and return the element
-    static int linearSearch2(int[] arr, int target){
-        if(arr.length == 0){
+    // search the target and return the element
+    static int linearSearch2(int[] arr, int target) {
+        if (arr.length == 0) {
             return -1;
         }
 
-        //for loop for search
-        for(int element : arr){
-            if(element == target){
+        // run a for loop
+        for (int element : arr) {
+            if (element == target) {
                 return element;
             }
         }
-
-        //this statement will execute if the none of the above return statement is execute
-        //hence the target not found
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
         return Integer.MAX_VALUE;
     }
+
 
     //search in the array return the index of the element found
     //otherwise item not found return -1
