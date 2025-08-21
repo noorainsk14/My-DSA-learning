0aa823d7b75ead07396370e092ff3d692c485552
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-08-21 22:35:36 +0530

    solved some question

---
+++ BinarySearch/src/FirstAndLastIndex.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class FirstAndLastIndex {
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] ans = searchRange(nums, target);
+        System.out.println(Arrays.toString(ans));
+
+    }
+    public static int[] searchRange(int[] nums, int target) {
+            int[] ans = {-1, -1};
+            // check for first occurrence if target first
+            ans[0] = search(nums, target, true);
+            if (ans[0] != -1) {
+                ans[1] = search(nums, target, false);
+            }
+            return ans;
+    }
+    public static int  search(int[] nums , int target, boolean firstIndex){
+            int ans = -1;
+            int start = 0;
+            int end = nums.length - 1;
+
+            while(start <= end){
+                int mid = start + (end - start)/2;
+
+                if(target < nums[mid]){
+                    end = mid-1;
+                }else if(target > nums[mid]){
+                    start = mid+1;
+                }else{
+                    //potential ans found
+                    ans= mid;
+                    if(firstIndex){
+                        end = mid-1;
+                    }else{
+                        start = mid +1;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+
+
