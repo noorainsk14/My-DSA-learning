f9572c4659f79ecff79d00cada9f6ffe24dd2518
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-08-22 23:05:59 +0530

    Solve some more question on rotated binary search

--- BinarySearch/src/CountRotation.java
+++ BinarySearch/src/CountRotation.java
@@ -1,51 +1,16 @@
-public class RBS {
+public class CountRotation {
     public static void main(String[] args) {
-    int[] nums = {4,5,6,7,0,1,2};
-        System.out.println(findPivot(nums));
+
+        int[] arr = {4,4,5,6,7,0,1,2,2,3};
+        System.out.println(countRotation(arr));
     }
 
-    public static  int search(int[] nums, int target){
-        int pivot = findPivot(nums);
-
-        // if you did not find a pivot, it means the array is not rotated
-        if(pivot == -1){
-            // just do normal binary search
-            return binarySearch(nums, target, 0, nums.length-1);
-        }
-        // if pivot is found, you have found 2 asc sorted arrays
-        if(nums[pivot] == target){
-            return pivot;
-        }
-        if(target >= nums[0]){
-            return binarySearch(nums, target, 0, pivot-1);
-        }
-
-        return binarySearch(nums, target, pivot+1, nums.length-1);
-
-
+    public static int countRotation(int[] arr){
+        int pivot = findPivotWithDuplicates(arr);
+        return pivot+1;
     }
 
-    static int binarySearch(int[] arr, int target, int start, int end){
-
-
-
-        while (start <= end) {
-            //find the middle element
-            int mid = start + (end - start)/2;
-
-
-            if(target < arr[mid]){
-                end = mid-1;
-            } else if (target > arr[mid]) {
-                start = mid+1;
-            }else {
-                //ans found
-                return mid;
-            }
-        }
-        return -1;
-    }
-
+    //for distinct values
     public static int findPivot(int[] arr){
         int start = 0;
         int end = arr.length-1;
@@ -69,7 +34,7 @@ public class RBS {
         }
         return -1;
     }
-
+// for duplicates value
     public static int findPivotWithDuplicates(int[] arr){
         int start = 0;
         int end = arr.length-1;
@@ -116,5 +81,4 @@ public class RBS {
         }
         return -1;
     }
-
 }
