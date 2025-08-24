85235835def4a9cf2eabe8216552e56efc121cbb
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-08-24 20:57:09 +0530

    solve question on sorting

---
+++ Sorting/src/FindAllMissing.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllMissing {
+    public static void main(String[] args) {
+        int[] nums = {3,4,2,1,8,9,5,3,4};
+        System.out.println(findDisappearedNumbers(nums));
+
+    }
+
+    public static List<Integer> findDisappearedNumbers(int[] nums){
+        int i = 0;
+        while (i < nums.length){
+            int correct = nums[i] -1;
+            if(nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            }else{
+                i++;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for(int index = 0; index < nums.length; index++){
+            if (nums[index] != index + 1){
+                ans.add(index + 1);
+            }
+        }
+        return ans;
+    }
+
+     static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+}
