8d028434ba3e8f37865a5f0621f10d17ed2bade0
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-08-28 22:41:37 +0530

    Solved fibo question with recursion

---
+++ Recursion/src/Fibo.java
@@ -0,0 +1,17 @@
+public class Fibo {
+
+    public static void main(String[] args) {
+        int n = 7;
+        int ans = fib(n);
+        System.out.println(ans);
+    }
+
+    static int fib(int n){
+
+        if( n<2){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+}
