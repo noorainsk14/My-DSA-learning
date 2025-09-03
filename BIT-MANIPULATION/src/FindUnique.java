d27f75a57b5923ae3ab7bdd4816c42c7c852af92
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-09-03 22:00:45 +0530

    solve question using xor operator

---
+++ BIT-MANIPULATION/src/FindUnique.java
@@ -0,0 +1,17 @@
+public class FindUnique {
+
+    public static void main(String[] args) {
+        int [] arr = {2,4,3,6,2,4,3,};
+        System.out.println(ans(arr));
+    }
+
+    private static int ans(int[] arr) {
+
+        int unique = 0 ;
+
+        for (int n : arr){
+            unique ^= n;
+        }
+        return unique;
+    }
+}
